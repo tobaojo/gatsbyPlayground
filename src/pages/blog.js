a058fb92d51b017e33678b544fbc201dcc50cd81
Toba <toba_ojo@hotmail.com>
2020-11-22 19:54:30 +0000

    added functionality to dynamically add

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,17 +1,41 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { graphql, useStaticQuery } from 'gatsby'
 
 
-const blogPage = () => {
+const BlogPage = () => {
+
+    const data = useStaticQuery(graphql`query{
+        allMarkdownRemark{
+          edges{
+            node{
+              frontmatter{
+                title
+                date
+              }
+            }
+          }
+        }
+      }`);
+
 
     return (
       
             <Layout>
             <h1>Blog</h1>
-            <p>posts will show up here...</p>
+            <ol>
+                {data.allMarkdownRemark.edges.map((edge) =>{
+                    return(
+                        <li>
+                            <h2>{edge.node.frontmatter.title}</h2>
+                            <p>{edge.node.frontmatter.date}</p>
+                        </li>
+                    )
+                })}
+            </ol>
             </Layout>
         
     )
 }
 
-export default blogPage;
+export default BlogPage;
---
+++ src/templates/blog.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const Blog = () =>{
+return(
+    <Layout>
+        this is the blog template
+    </Layout>
+    )
+}
+export default Blog;
